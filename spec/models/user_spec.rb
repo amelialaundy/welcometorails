8a0cc85e8d98a4ba73960f03a7a5b19d47e16323
Amelia Laundy <apprentice@Samoa.local>
2014-09-17 10:51:28 +1200

    trsting for User model new

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   describe do
-    it "a new user is initialized" do
+    it "initializes a new user" do
       user = User.new
       expect(user).to be_a(User)
+    end
+
+    it "creates a cart for each new user" do
+      user = User.create
+      expect(user.cart).to be_instance_of(Cart)
     end
   end
 end
