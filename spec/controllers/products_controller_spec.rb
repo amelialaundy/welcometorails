e77680dcc73408f90a8e6e557651ffab2335116a
Amelia Laundy <apprentice@Samoa.local>
2014-09-17 16:42:33 +1200

    test passing for products controller post create method

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, :type => :controller do
+   let(:product) { create(:product) }
+  describe "GET index method" do
+    it "displays all products" do
+      get :index
+      expect(response).to render_template(:index)
+    end
 
+    it "assigns @products" do
+      get :index
+      expect(assigns(:products)).to eq([product])
+    end
+  end
+
+  describe "POST create method" do
+
+    it "creates a new product" do
+      expect{
+        post :create, product: FactoryGirl.attributes_for(:product)
+      }.to change(Product, :count).by(1)
+    end
+
+    it "redirects to the product#show" do
+      post :create, product: FactoryGirl.attributes_for(:product)
+      expect(response).to redirect_to(assigns(:product))
+    end
+
+  end
 end
