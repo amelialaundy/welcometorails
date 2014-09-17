e9627b61e5583311a1fad8d9b4b5ce567eb3b2bd
Amelia Laundy <apprentice@Samoa.local>
2014-09-17 20:17:43 +1200

    testing products edit method without hitting the db

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -32,8 +32,47 @@ RSpec.describe ProductsController, :type => :controller do
       post :create, product: FactoryGirl.attributes_for(:product)
       expect(response).to redirect_to(assigns(:product))
     end
+  end
+
+  describe "GET new method" do
+    it "renders a new product form" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "GET edit method" do
 
 
+
+
+    context "hitting the db" do
+      let(:product) { create(:product) }
+      before(:each) do
+        get :edit, id: product
+      end
+      it "renders an edit product form" do
+        expect(response).to render_template(:edit)
+      end
+
+      it "finds the right product to edit" do
+        expect(assigns(:product)).to eq(product)
+      end
+    end
+
+    context "not hitting the db, using doubles" do
+      let(:product) {double("product")}
+
+      it "finds the specific product" do
+        expect(Product).to receive(:find).once.and_return(product)
+        get :edit, id: product
+      end
+
+      it "renders an edit product form" do
+        get :edit, id: product
+        expect(response).to render_template(:edit)
+      end
+    end
 
   end
 end
