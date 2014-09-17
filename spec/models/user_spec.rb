c33f35963bb1b8918257379a8d9def7dca619a33
Amelia Laundy <apprentice@Samoa.local>
2014-09-17 12:23:23 +1200

    third passing test- user validations for new user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  describe do
-    let(:user) { create(:user) }
+  let(:user) { create(:user) }
+  describe "a new user" do
+
     it "initializes a new user" do
       expect(user).to be_a(User)
     end
@@ -10,5 +11,7 @@ RSpec.describe User, :type => :model do
     it "creates a cart for each new user" do
       expect(user.cart).to be_instance_of(Cart)
     end
+
+    it { should validate_presence_of(:email) }
   end
 end
